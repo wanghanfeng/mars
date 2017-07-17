e44fcd03f8f6ba41b3e5269648499da14ee1d8db
wanghanfeng <wanghanfeng@baidu.com>
2017-07-17 18:19:10 +0800

    build for ios_with_bitcode

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "dfebb49"
+#define MARS_REVISION "de1982c"
 #define MARS_PATH "master"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2017-01-10 21:41:50"
-#define MARS_TAG "samplelibs"
+#define MARS_BUILD_TIME "2017-07-17 18:12:40"
+#define MARS_TAG "tag_[2017-07-17_18.12]@git-de1982c@wanghanfeng"
 
 #endif
